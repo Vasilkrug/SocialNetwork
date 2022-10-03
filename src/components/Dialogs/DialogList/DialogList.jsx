76ddc3010799943a227bc61a28d6591f4807a3e9
vasilkrug <vkruglovec@gmail.com>
2022-10-03 21:29:44 +0300

    add new component DialogList

---
+++ src/components/Dialogs/DialogList/DialogList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from "./DialogList.module.scss";
+
+const  DialogList = (props) => {
+    return (
+        <ul className={style.dialog_list}>
+
+        </ul>
+    );
+}
+
+export default DialogList;
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './Dialogs.module.scss'
 import DialogForm from "./DialogForm/DialogForm";
+import DialogList from "./DialogList/DialogList";
 
 const Dialogs = () => {
     return (
@@ -8,22 +9,7 @@ const Dialogs = () => {
             <div className={style.dialogs}>
             <DialogForm/>
                 <hr/>
-                <ul className={style.dialog_list}>
-                    <li className={style.dialog_item}>
-                        <div className={style.dialog_photo}>
-                            <img src="/images/animal.jpeg"/>
-                        </div>
-                        <div className={style.dialog}>
-                            <div className={style.dialog_title}>
-                                <h2>Василий Кругловецкий</h2>
-                            </div>
-                            <div className={style.dialog_preview}>
-                                <span>какое то сообщение блабалалалалабалалаs</span>
-                            </div>
-                        </div>
-                    </li>
-
-                </ul>
+                <DialogList/>
             </div>
             <div className={style.chat_list}>
                 <div className={style.chat_item}>Привет</div>
