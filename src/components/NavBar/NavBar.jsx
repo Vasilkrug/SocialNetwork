97dcfd955598b719965f671dd6a10bf5a1688ff6
vasilkrug <vkruglovec@gmail.com>
2022-10-10 22:38:26 +0300

    add Friends link to NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,6 +7,7 @@ const NavBar = (props) => {
         <nav className={styles.nav}>
             <ul className={styles.nav_list}>
                 <li><img src="/images/icons/user.png" alt="user"/><Link to="/profile">Профиль</Link></li>
+                <li><img src="/images/icons/group.png" alt="friends"/>Друзья</li>
                 <li><img src="/images/icons/message.png" alt="message"/><Link to="/dialogs">Мои сообщения</Link></li>
                 <li><img src="/images/icons/news.png" alt="news"/><Link>Новости</Link></li>
                 <li><img src="/images/icons/music.png" alt="music"/><Link>Музыка</Link></li>
