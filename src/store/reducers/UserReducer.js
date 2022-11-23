b93498623035eebcbcf0d68715801026a44faaec
vasilkrug <vkruglovec@gmail.com>
2022-11-23 20:18:29 +0300

    add User Store

---
+++ src/store/reducers/UserReducer.js
@@ -0,0 +1,16 @@
+import {LOGIN, SET_USER} from "../actionsTypes/UserActionsTypes";
+
+const initialState = {
+    isLogin: false,
+    user: null
+}
+export const UserReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case LOGIN:
+            return {...state, isLogin: action.isLogin};
+        case SET_USER:
+            return {...state,user: action.user}
+        default:
+            return state;
+    }
+}
